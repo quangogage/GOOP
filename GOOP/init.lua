bb0ba6ff005cce0bbe89b8b8f06534bb2d056c67
quangogage <gage@quangoinc.com>
2023-08-16 09:14:06 -0700

    Added arguments, type checking is now optional

--- GOOP/init.lua
+++ GOOP/init.lua
@@ -11,17 +11,33 @@ local Goop = {}
 ---@param requiredParams table
 function checkParameters(params, requiredParams)
     for i=1,#requiredParams do
-        local key = requiredParams[i][1]
+        local key
+        if type(requiredParams[i]) == "table" then
+            key = requiredParams[i][1]
+        else
+            key = requiredParams[i]
+        end
         if key then
             local expectedType = requiredParams[i][2]
             if params[key] == nil then
-                error("Missing parameter '" .. key .. "'.")
-            elseif type(params[key]) ~= expectedType and params[key].type ~= expectedType then
-                error(key .. " is wrong type. Expected '" .. expectedType .. "'. Received '" .. type(params[key]) .. "'.")
+                error("GOOP: Missing parameter '" .. key .. "'.")
+            elseif expectedType ~= nil then ---If a type was specified for this parameter
+                local invalidType = true
+                if type(params[key]) ~= expectedType then ---If it doesn't have the correct type.
+                    if type(params[key]) == "table" then ---If it's a table, BUT it has a hardcoded `.type` value that matches the expected type.
+                        if params[key].type == expectedType then
+                            invalidType = false
+                        end
+                    end
+                    if invalidType then
+                        error("GOOP: " .. key .. " is wrong type. Expected '" .. expectedType .. "'. Received '" .. type(params[key]) .. "'.")
+                    end
+                end
             end
         end
     end
 end
+
 
 ---Define a new class
 ---@param data {static?: table, dynamic?: table, extends?: table, parameters: table[]}
@@ -41,19 +57,59 @@ function Goop.Class(data)
 
     ---Metatable
     local mt = {
-        __call = function(_,params)
-            local params = params or {}
-            checkParameters(params,newClassDefinition.requiredParameters)
+        __call = function(_,...)
+            local passedState
+
+            ---Parameters
+            if data.parameters then
+                local parameters = ...
+                assert(... ~= nil, "GOOP: Class insantiated without required parameters")
+                assert(type(...) == "table", "GOOP: Class expected parameters (table), received: " .. type(...))
+                checkParameters(..., data.parameters)
+                passedState = ...
+
+            ---Arguments
+            elseif data.arguments then
+                local args = {...}
+                passedState = {}
+                for i=1,#data.arguments do
+                    local key = data.arguments[i]
+                    passedState[key] = arg[i]
+                    if args[i] == nil then
+                        error("GOOP: Missing arguments, expected " .. #data.arguments .. ". Received " .. #args)
+                    elseif type(data.arguments[i]) == "table" then ---If a type was specified and needs to be checked for this argument.
+                        local expectedType = data.arguments[i][2]
+                        if type(args[i]) ~= expectedType then
+                            local invalidType = true
+                            if type(args[i]) == "table" then
+                                if args[i].type == expectedType then
+                                    invalidType = false
+                                end
+                            end
+                            if invalidType then
+                                error("GOOP: Argument #" .. i .. " is wrong type. Expected '" .. expectedType .. "'. Received '" .. type(args[i]) .. "'.")
+                            end
+                        end
+                    else
+                        passedState[key] = args[i]
+                    end
+                end
+            end
+
             ---Create new references for all dynamic data - but use __index fallback for static.
             local newInstance
             if data.extends then
-                newInstance = data.extends(params)
+                ---Create a new instance of the parent class
+                newInstance = data.extends(...)
                 util.table.deepCopy(newInstance,state.dynamic)
                 util.table.copy(newInstance,state.static)
             else
+                ---Create new references of the dynamic state values.
                 newInstance = util.table.createDeepCopy(state.dynamic)
             end
-            util.table.copy(newInstance,params)
+            util.table.copy(newInstance, passedState)
+
+        
             setmetatable(newInstance, {__index = newClassDefinition})
             return newInstance
         end
